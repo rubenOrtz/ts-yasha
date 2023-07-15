8176abd7a0f1f3734434ef45030c769b76bf035b
eliyya <tecno_eli@hotmail.com>
2023-07-15 00:03:59 -0600

    index migrated

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import Source from './Source.cjs'
+import Track from './Track.cjs'
+import TrackPlayer from './TrackPlayer.cjs'
+import VoiceConnection from './VoiceConnection.cjs'
+
+import Youtube from './api/Youtube.cjs'
+import Soundcloud from './api/Soundcloud.cjs'
+import Spotify from './api/Spotify.cjs'
+
+export {
+    Source,
+    Track,
+    TrackPlayer,
+    VoiceConnection
+}
+
+export default {
+    Youtube,
+    Soundcloud,
+    Spotify
+}
